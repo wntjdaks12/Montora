f5fcb3f68a621494c28498858a0e5ca0b1f3d99f
wntjdaks12 <qqqq8692@naver.com>
2021-11-24 03:19:13 +0900

    벽돌 스크립트 추가

---
+++ Assets/Scripts/Brick/Brick.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 벽돌 추상 클래스
+/// </summary>
+public abstract class Brick : MonoBehaviour
+{
+    public PoolableObject poolableObject;
+    public ParticleSystem ps;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Ball")
+        {
+            ps.transform.parent = null;
+
+            ps?.Play();
+
+            poolableObject?.EnQueue();
+        }
+    }
+}
